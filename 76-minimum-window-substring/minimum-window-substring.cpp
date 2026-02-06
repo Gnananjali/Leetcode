dbe8253e3e4a98a2e1f0a4564ab8d19340c6a212
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-02-06 21:12:48 +0530

    Update minimum-window-substring.cpp

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -32,3 +32,38 @@ public:
         return minLen == INT_MAX?"":s.substr(start, minLen);
     }
 };
+
+
+
+Algorithm:
+• Create a map need to store frequency of characters in string t
+
+• Count how many total characters are needed → count = size of t
+
+• Initialize:
+  left = 0 (start of window)
+  minLen = infinity
+  start = 0 (starting index of best window)
+
+• Loop with right from 0 to end of string s
+
+  • If current character is needed (need[s[right]] > 0)
+    → Decrease count
+
+  • Decrease frequency in map → need[s[right]]--
+
+• When count == 0 (valid window found):
+
+  • Check if current window size is smaller than minLen
+    → Update minLen and start
+
+  • Try to shrink window from left:
+    → Increase need[s[left]]++
+    → If this character becomes required again (need[s[left]] > 0)
+      • Increase count
+
+  • Move left++
+
+• After loop ends:
+  • If no window found → return empty string
+  • Else return substring starting at start of length minLen
