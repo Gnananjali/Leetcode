17fd729e160026265b8b57994777874856d0d1bb
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-04-02 21:49:12 +0530

    Time: 3 ms (67.31%) | Memory: 14.8 MB (36.21%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> set;
-        for (int i = 0; i < nums.size(); i++) {
+        unordered_map<int, int>set;
+        for(int i=0;i<nums.size();i++){
             int need = target - nums[i];
 
-            if (set.count(need)) {
+            if(set.count(need)){
                 return {i, set[need]};
             }
             set[nums[i]] = i;
@@ -13,22 +13,3 @@ public:
         return {};
     }
 };
-
-
-
-Algorithm:
-
-• Create an unordered map to store number and its index
-
-• Loop through the array from i = 0 to n-1
-
-  • Calculate the required number
-    → need = target - nums[i]
-
-  • If need exists in the map
-    → Return the indices {i, map[need]}
-
-  • Else store current number and index in map
-    → map[nums[i]] = i
-
-• If no pair is found, return empty vector
