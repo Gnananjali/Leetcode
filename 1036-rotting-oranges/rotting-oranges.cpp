556435d4ce3aaa4e70b8dce2ef9289c751945d58
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-04-19 16:13:30 +0530

    Time: 1 ms (37.62%) | Memory: 16.8 MB (52.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-
-        queue<pair<int,int>>q;
+        int n= grid.size(), m = grid[0].size();
         int fresh = 0;
 
+        if(n == 0 && m == 0){
+            return 0;
+        }
+
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }
-                else if(grid[i][j]==1){
+                if(grid[i][j]==1){
                     fresh++;
+                }
+                else if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
@@ -21,26 +24,24 @@ public:
         vector<int> dx = {1,-1,0,0};
         vector<int> dy = {0,0,1,-1};
 
-        while(!q.empty() && fresh >0){
+        while(!q.empty() && fresh>0){
             int size = q.size();
-            minutes++;
-
             for(int i=0;i<size;i++){
                 auto[x,y] = q.front();
                 q.pop();
 
                 for(int d=0;d<4;d++){
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
-
-                    if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1){
-                        grid[nx][ny] = 2;
+                    int ni = x + dx[d];
+                    int nj = y + dy[d];
+                    if(ni<n && nj<m && ni>=0 && nj>=0 && grid[ni][nj]==1){
+                        grid[ni][nj]=2;
                         fresh--;
-                        q.push({nx,ny});
+                        q.push({ni,nj});
                     }
                 }
             }
+            minutes++;
         }
-        return (fresh == 0)?minutes:-1;
+        return (fresh==0)?minutes:-1;
     }
 };
