e1b6b6e5f95fc3aeb2811d36c0e3a6b5c33330f6
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-04-19 15:11:20 +0530

    Time: 13 ms (10.30%) | Memory: 30.9 MB (25.99%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -3,38 +3,39 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int maxVal = 0;
-
-        if(n==0 && m==0){
+        int maxArea = 0;
+        if(n ==0 && m == 0){
             return 0;
         }
-        function <void(int,int,int&)> dfs = [&](int i, int j,int &count){
-            if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0){
-                return;
-            }
-            grid[i][j]=0;
-            count++;
 
-            vector<int> dx = {1,-1,0,0};
-            vector<int> dy = {0,0,1,-1};
+    function<int(int,int)> dfs = [&](int i, int j){
+        if(i<0 || i>=n || j<0 || j>=m || grid[i][j]==0){
+            return 0;
+        }
+        grid[i][j] = 0;
 
-            for(int d=0;d<4;d++){
-                int ni = i+dx[d];
-                int nj = j+dy[d];
+    int area = 1;
+    vector<int> dx = {1,-1,0,0};
+    vector<int> dy = {0,0,1,-1};
+    
+    for(int d=0;d<4;d++){
+        int ni = i + dx[d];
+        int nj = j + dy[d];
 
-                
-                    dfs(ni,nj,count);
-                }
-            };
-        for(int i=0;i<n;i++){
+        area += dfs(ni, nj);
+
+    }
+    return area;
+    };
+
+    for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1){
-                    int count=0;
-                    dfs(i,j,count);
-                    maxVal = max(maxVal, count);
-                }
+                int area = dfs(i, j);
+                maxArea = max(maxArea, area);
             }
         }
-        return maxVal;
+        }
+        return maxArea;
     }
 };
