d0e9e048cce071bc88dc1c02befa32215fc6af14
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-05-17 16:20:23 +0530

    Time: 64 ms (94.64%) | Memory: 98.7 MB (46.10%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-      unordered_set<int> st;
-      for(int i=0;i<nums.size();i++){
-        if(st.count(nums[i])){
-            return true;
-        }else{
-            st.insert(nums[i]);
+        unordered_map<int, int> map;
+        for(int i=0;i<nums.size();i++){
+            int need = k - i;
+            if(map.count(nums[i]) && i-map[nums[i]] <= k){
+                return true;
+            }
+            map[nums[i]] = i;
         }
-        if(i>=k){
-            st.erase(nums[i-k]);
-        }
-      }
-      return false;
+        return false;
     }
 };
