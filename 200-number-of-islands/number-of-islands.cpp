5ec33716f60910db6e63cb2ef91a81b6e0dbe4d9
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-06-09 15:56:15 +0530

    Time: 23 ms (83.60%) | Memory: 16.6 MB (62.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,50 +1,30 @@
 class Solution {
 public:
-/* 
-in this code we need to take n and m as size of rows and columns of the grid and initilaize count as 0.
+void dfs(int i, int j, vector<vector<bool>>& vis, vector<vector<char>>& grid, int m, int n){
+    if(i<0||j<0||i>=m||j>=n||vis[i][j]||grid[i][j]!='1') return;
 
-if n and m are 0 then return 0.
+    vis[i][j] = true;
 
-we will create a function for dfs with parameters as i and j and here '&' is used for representing every i and j in the grid.
-
-we will check if i and j are less than 0 or greater than their sizes (n and m), if it is true will return or else mark the grid[i][j] as 0. It means we have visited the node and marked it as 0.
-
-then we will check the sides of the node which we visited up,down,left and right and same goes to them if they are equal to 1 we will mark them as 0.
-
-for i and j if grid[i][j] is 1 we will increment the count and transfer the i and j to the dfs function.
-
-at last return count.
-
-*/
+    dfs(i+1, j, vis, grid, m, n);
+    dfs(i-1, j, vis, grid, m, n);
+    dfs(i, j+1, vis, grid, m, n);
+    dfs(i, j-1, vis, grid, m, n);
+}
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int count = 0;
-        if(n == 0 && m == 0){
-            return 0;
-        }
-        function<void(int,int)> dfs = [&](int i, int j){
-            if(i<0 ||i>=n || j<0 || j>=m || grid[i][j]=='0'){
-                return;
-            }
-            grid[i][j] = '0';
+        int island = 0;
+        int m = grid.size();
+        int n = grid[0].size();
 
-            dfs(i+1, j);
-            dfs(i, j+1);
-            dfs(i-1, j);
-            dfs(i, j-1);
-        };
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
 
-
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    count++;
-                    dfs(i,j);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    dfs(i, j, vis, grid, m, n);
+                    island++;
                 }
             }
         }
-        return count;
+        return island;
     }
 };
