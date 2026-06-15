5d53c982d5284565dcab25fd5607b3582f1c9132
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-06-15 16:57:33 +0530

    Time: 265 ms (65.67%) | Memory: 298.6 MB (57.73%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-
-    bool dfs(int node, int destination, vector<vector<int>>& adj, vector<int>& vis){
+    bool dfs(int node, int destination, vector<vector<int>>& adj, vector<bool>& vis){
         if(node == destination) return true;
-        vis[node] = 1;
+
+        vis[node] = true;
         for(int i:adj[node]){
             if(!vis[i]){
-                if(dfs(i, destination,adj, vis)) return true;
+                if(dfs(i, destination, adj, vis))
+                return true;
             }
         }
         return false;
     }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-       
         vector<vector<int>> adj(n);
-        for(auto &e:edges){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        vector<bool> vis(n, false);
+        for(auto &i:edges){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+
         }
-            vector<int> vis(n,0);
-            return dfs(source, destination, adj, vis);
-        
+        return dfs(source, destination, adj, vis);
     }
 };
