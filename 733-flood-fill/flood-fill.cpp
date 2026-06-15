107f809f03627dd0eff7457e137480034e14fec9
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-06-15 17:12:27 +0530

    Time: 2 ms (17.23%) | Memory: 18.1 MB (76.31%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int i, int j, int oldColor, int newColor){
+    void dfs(vector<vector<int>>& image, int r, int c, int oldColor, int newcolor){
         int m = image.size();
         int n = image[0].size();
-        if(i < 0 || j < 0 || i >= m || j >= n) return;
 
-        if(image[i][j] != oldColor) return;
-        image[i][j] = newColor;
+        if(r<0||c<0||r>=m||c>=n) return;
+        if(image[r][c] != oldColor)
+        return;
+        image[r][c] = newcolor;
 
-        dfs(image, i+1, j, oldColor, newColor);
-        dfs(image, i-1, j, oldColor, newColor);
-        dfs(image, i, j+1, oldColor, newColor);
-        dfs(image, i, j-1, oldColor, newColor);
+        dfs(image, r+1, c, oldColor, newcolor);
+        dfs(image, r-1, c, oldColor, newcolor);
+        dfs(image, r, c+1, oldColor, newcolor);
+        dfs(image, r, c-1, oldColor, newcolor);
+
+        
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int oldColor = image[sr][sc];
