ba177df8fea5748efd84f38f33f52feb3f5f9060
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-06-19 12:05:49 +0530

    Time: 5 ms (75.38%) | Memory: 42.1 MB (92.50%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,29 +1,42 @@
+class DSU {
+public:
+    vector<int> parent, size;
+    DSU(int n){
+        parent.resize(n);
+        size.resize(n, 1);
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        }
+    }
+    int find(int x){
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    bool unite(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+        if(pu == pv) return false;
+        if(size[pu]<size[pv])
+        swap(pu,pv);
+        parent[pv] = pu;
+        size[pu] += size[pv];
+        return true;
+    }
+};
+
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& vis){
-        vis[node] = 1;
-        for(int i : adj[node]){
-            if(!vis[i])
-                dfs(i, adj, vis);
-            }
-        }
-    
     int makeConnected(int n, vector<vector<int>>& connections) {
-        vector<int> vis(n,0);
+        DSU dsu(n);
+        int components = n;
         if(connections.size() < n-1) return -1;
-
-        vector<vector<int>> adj(n);
-        for(auto &e : connections){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-        }
-            int num = 0;
-            for(int i=0;i<n;i++){
-                if(!vis[i]){
-                    num++;
-                    dfs(i, adj, vis);
-                }
+        for(auto& e:connections){
+            int u = e[0];
+            int v = e[1];
+            if(dsu.unite(u,v)){
+                components--;
             }
-        return num-1;
+        }
+        return components-1;
     }
 };
