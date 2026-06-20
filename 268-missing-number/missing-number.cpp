90207b79473abeafefbd2252906b282551196af7
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-06-20 12:46:20 +0530

    Time: 14 ms (9.85%) | Memory: 28 MB (5.00%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_set<int> map(nums.begin(), nums.end());
+        unordered_set<int> mp(nums.begin(), nums.end());
 
-        for(int i=0;i<=nums.size()+1;i++){
-            if(!map.count(i)){
+        for(int i=0;i<=nums.size();i++){
+            if(!mp.count(i)){
                 return i;
             }
+            mp.insert(i);
         }
         return {};
     }
