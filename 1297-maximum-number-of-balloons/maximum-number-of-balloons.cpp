3148d672328653cad850bc4114876c78ae4f8d32
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-06-22 21:53:49 +0530

    Time: 3 ms (12.33%) | Memory: 9 MB (39.61%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char, int>mp;
+        for(char ch:text){
+            if(ch=='b' || ch=='a' || ch=='l' || ch=='o' || ch=='n' ){
+            mp[ch]++;
+        }else{
+            continue;
+        }
+        }
+        return min({
+            mp['b'],
+            mp['a'],
+            mp['l']/2,
+            mp['o']/2,
+            mp['n']
+        });
+        
+    }
+};
