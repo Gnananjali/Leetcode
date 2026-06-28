664cf29ff2201de7e673ad9d4051f543fec5c074
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-06-28 11:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (7.41%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,14 +4,14 @@ public:
         unordered_map<char, char> mp;
         unordered_set<char> used;
         for(int i=0;i<s.size();i++){
-        if(mp.count(s[i])){
-            if(mp[s[i]]!=t[i])
-            return false;
-        }else{
-            if(used.count(t[i])) return false;
-            mp[s[i]] = t[i];
-            used.insert(t[i]);
-        }
+            if(mp.count(s[i])){
+                if(mp[s[i]]!=t[i])
+                return false;
+            }else{
+                if(used.count(t[i]))  return false;
+                mp[s[i]] = t[i];
+                used.insert(t[i]);
+            }
         }
         return true;
     }
