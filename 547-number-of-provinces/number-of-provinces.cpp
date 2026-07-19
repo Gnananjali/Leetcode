2a1e81c97aa2704792340929ccdee341cf388e0a
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-07-19 17:28:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (83.30%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,22 +1,51 @@
-class Solution {
+class DisjointSet {
 public:
-    void dfs(int city, vector<vector<int>>& isConnected, vector<bool>& visited){
-        int n = isConnected.size();
-        visited[city] = true;
-        for(int neighbor=0;neighbor<n;neighbor++){
-            if(isConnected[city][neighbor]==1 && !visited[neighbor]){
-                dfs(neighbor, isConnected, visited);
+vector<int> parent, size;
+        
+        DisjointSet(int n){
+            parent.resize(n+1);
+            size.resize(n+1, 1);
+            for(int i=0;i<=n;i++){
+                parent[i] = i;
             }
         }
-    }
+        int findParent(int node){
+            if(parent[node]==node) {
+            return node;
+        }
+            return parent[node] = findParent(parent[node]);
+        }
+
+        void UnionbySize(int u, int v){
+            int parentU = findParent(u);
+            int parentV = findParent(v);
+            if(parentU == parentV) return;
+            if(size[parentU] < size[parentV]){
+                parent[parentU] = parentV;
+                size[parentV] += size[parentU];
+            }else{
+                parent[parentV] = parentU;
+                size[parentU] += size[parentV];
+            }
+        }
+};
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool> visited(n, false);
+        DisjointSet ds(n);
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    ds.UnionbySize(i,j);
+                }
+            }
+        }
         int provinces=0;
-        for(int city=0;city<n;city++){
-            if(!visited[city]){
+        for(int i=0;i<n;i++){
+            if(ds.findParent(i)==i){
                 provinces++;
-                dfs(city, isConnected, visited);
             }
         }
         return provinces;
