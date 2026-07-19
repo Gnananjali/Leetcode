103dded5e3ded810537434c53f14f9e425d19c91
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-07-19 11:41:47 +0530

    Time: 51 ms (99.90%) | Memory: 44.4 MB (53.19%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,47 +1,49 @@
 class Solution {
 public:
-    vector<int> dijkstra(int start, vector<vector<pair<int, int>>>& adj){
-        int n = adj.size();
-        vector<int> dist(n, INT_MAX);
-        priority_queue<
-            pair<int, int>,
-            vector<pair<int, int>>,
-            greater<pair<int, int>>> pq;
-        dist[start] = 0;
-        pq.push({0, start});
-        while(!pq.empty()){
-            auto[d, node] = pq.top();
-            pq.pop();
-            if(d > dist[node]) continue;
-            for(auto[nei, wt] : adj[node]){
-                int newDist = d+wt;
-                if(newDist < dist[nei]){
-                    dist[nei]=newDist;
-                    pq.push({newDist, nei});
-                }
-            }
-        }
-        return dist;
-
-    }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n+1);
-        for(auto& t: times){
-            int u = t[0];
-            int v = t[1];
-            int w = t[2];
+        vector<vector<pair<int,int>>> adj(n+1);
+
+        for(auto &it:times){
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
 
             adj[u].push_back({v,w});
         }
-        vector<int> dist = dijkstra(k, adj);
-        int ans = 0;
+        vector<int> distance(n+1, INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+
+        distance[k] = 0;
+        pq.push({0,k});
+
+        while(!pq.empty()){
+            auto curr = pq.top();
+            pq.pop();
+
+            int dist = curr.first;
+            int node = curr.second;
+
+            if(dist > distance[node]){
+                continue;
+            }
+            for(auto &it:adj[node]){
+                int neighbor = it.first;
+                int weight = it.second;
+
+                if(distance[node] + weight < distance[neighbor]){
+                    distance[neighbor] = distance[node] + weight;
+
+                    pq.push({distance[neighbor], neighbor}); 
+                }
+            }
+        }
+        int ans=0;
         for(int i=1;i<=n;i++){
-            if(dist[i] == INT_MAX){
+            if(distance[i] == INT_MAX){
                 return -1;
             }
-                ans = max(ans, dist[i]);
-            }
-            return ans;
+            ans = max(ans, distance[i]);
         }
-    
+        return ans;
+    }
 };
