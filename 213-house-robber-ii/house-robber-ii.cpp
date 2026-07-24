c71c825665ed6c2511aa3710eb988cc53ec8706d
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-07-24 15:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (74.46%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int robLine(vector<int>& nums, int start, int end){
-        int prev1 = 0;
-        int prev2 = 0;
-        for(int i=start;i<=end;i++){
-            int curr = max(prev1, nums[i]+prev2);
+    int robRange(vector<int>& nums, int start, int end){
+        if(start == end) return nums[start];
+        int prev2 = nums[start];
+        int prev1 = max(nums[start], nums[start+1]);
+
+        for(int i=start+2;i<=end;i++){
+            int current = max(prev1, nums[i]+prev2);
             prev2 = prev1;
-            prev1 = curr;
+            prev1 = current;
         }
         return prev1;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1) return nums[0];
+        if(n==1) return nums[0];
 
-        int case1 = robLine(nums, 0, n-2);
-        int case2 = robLine(nums, 1, n-1);
-        return max(case1, case2);
+        return max(
+            robRange(nums, 0, n-2),
+            robRange(nums, 1, n-1)
+        );
     }
-    
 };
