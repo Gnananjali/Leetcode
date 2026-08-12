97b0fdc8eb59bb7a8b2bf3a20eac82464271513d
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-08-12 12:00:37 +0530

    Time: 38 ms (32.98%) | Memory: 92 MB (60.92%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -7,13 +7,11 @@ public:
     
     int next(int price) {
         int span = 1;
-        
         while(!st.empty() && price >= st.top().first){
             span += st.top().second;
             st.pop();
         }
         st.push({price, span});
-        
         return span;
     }
 };
