33dd5586fa461d017dbf13fc92f447bfe5442871
Gnananjali <136087716+Gnananjali@users.noreply.github.com>
2026-09-16 16:56:27 +0530

    Time: 11 ms (68.63%) | Memory: 51.9 MB (84.04%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& state){
+        
+        if(state[node]==2) return true;
+        if(state[node] == 3) return false;
+        if(state[node] == 1) return false;
+
+        state[node]=1;
+        for(int nei : graph[node]){
+            
+            if(!dfs(nei, graph, state)) {
+                state[node] = 3;
+            return false;
+            }
+        }
+        state[node]=2;
+        return true;
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> state(n, 0);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+           
+            if(dfs(i, graph, state)){
+                ans.push_back(i);
+            
+        }
+        
+    }
+    return ans;
+    }
+};
